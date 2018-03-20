b3c3900de64b3ba2bdfa094d1225a6a4ad1fb097
FeastInc <feast.inc.original@gmail.com>
2018-03-20 15:15:27 +0600

    Fix filter and reduce

--- filterReduce.c
+++ filterReduce.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 void filter(
         void *from,
         void *to,
         size_t count,
         size_t size,
-        int (*filter)(void *, void *)
+        int (*filter)(void *)
 ) {
-    char *from_bytes = (char*)from;
-    char *to_bytes = (char*)to;
+    char *from_bytes = (char *) from;
+    char *to_bytes = (char *) to;
 
     size_t count_to = 0;
 
     for (size_t i = 0; i < count; i++) {
-        int res = filter(from_bytes + (size * i), to_bytes + (count_to * size));
+        int res = filter(from_bytes + (size * i));
         if (res) {
+            memcpy(to_bytes + (count_to * size), from_bytes + (size * i), size);
             count_to++;
         }
     }
@@ -22,30 +24,32 @@ void filter(
 
 void reduce(
         void *from,
+        void *result,
         size_t count,
         size_t size,
         void (*func)(void *, void *)
 ) {
-    char *from_bytes = (char*)from;
+    char *from_bytes = (char *) from;
+    char *result_bytes = (char *) result;
+
+    memcpy(result_bytes, from_bytes, size);
 
     for (size_t i = 1; i < count; i++) {
-        func(from_bytes, from_bytes + (size * i));
+        func(result_bytes, from_bytes + (size * i));
     }
 }
 
 void func_mul_int(void *a, void *b) {
-    int *a_i = (int*)a;
-    int *b_i = (int*)b;
+    int *a_i = (int *) a;
+    int *b_i = (int *) b;
 
     *a_i = *a_i * *b_i;
 }
 
-int filter_int(void *a, void *b) {
-    int *a_i = (int*)a;
-    int *b_i = (int*)b;
+int filter_int(void *a) {
+    int *a_i = (int *) a;
 
     if (*a_i > 0) {
-        *b_i = *a_i;
         return 1;
     }
 
@@ -65,8 +69,9 @@ int main(void) {
     printf("\n");
 
     int c[] = {1, 2, 3, 4, 5};
-    reduce(c, 5, sizeof(int), func_mul_int);
-    printf("%d\n", c[0]);
+    int result = 0;
+    reduce(c, &result, 5, sizeof(int), func_mul_int);
+    printf("%d\n", result);
 
     return 0;
 }
